47831c0492fa64dbabc6dc8abbfb6ee0718df5b8
TomaszPitak <tomekpitak2000@poczta.fm>
2023-04-12 23:34:35 +0200

    Empty project to fix package name problem

--- src/main/java/com/example/hurtownia/domain/AbstractReport.java
+++
@@ -1,8 +0,0 @@
-package com.example.hurtownia.domain;
-
-import java.io.FileNotFoundException;
-
-public abstract class AbstractReport {
-
-    public abstract void generateReport(String path, String title) throws FileNotFoundException;
-}
