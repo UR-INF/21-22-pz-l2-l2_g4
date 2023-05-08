73c99e2caa5fc9aac2d94e395acbd94c6ee6c902
TomaszPitak <tomekpitak2000@poczta.fm>
2023-05-08 13:37:26 +0200

    HUR-47 - Adding tests for the default option

--- src/test/java/com/example/hurtownia/domain/order/DiscountConverterTest.java
+++ src/test/java/com/example/hurtownia/domain/order/DiscountConverterTest.java
@@ -9,8 +9,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -43,35 +42,41 @@ class DiscountConverterTest {
     void fromNumericToPercentage() {
         assertThat(DiscountConverter.fromNumericToPercentage(code1Numeric)).isEqualTo(code1Percentage);
         assertThat(DiscountConverter.fromNumericToPercentage(code2Numeric)).isEqualTo(code2Percentage);
+        assertThat(DiscountConverter.fromCodeToPercentage("anyOther")).isEqualTo(codePercentageDefault);
     }
 
     @Test
     void fromNumericToCode() {
         assertThat(DiscountConverter.fromNumericToCode(code1Numeric)).isEqualTo(code1);
         assertThat(DiscountConverter.fromNumericToCode(code2Numeric)).isEqualTo(code2);
+        assertThat(DiscountConverter.fromNumericToCode(0.0)).isEqualTo(codeDefault);
     }
 
     @Test
     void fromPercentageToCode() {
         assertThat(DiscountConverter.fromPercentageToCode(code1Percentage)).isEqualTo(code1);
         assertThat(DiscountConverter.fromPercentageToCode(code2Percentage)).isEqualTo(code2);
+        assertThat(DiscountConverter.fromPercentageToCode("anyOther")).isEqualTo(codeDefault);
     }
 
     @Test
     void fromPercentageToNumeric() {
         assertThat(DiscountConverter.fromPercentageToNumeric(code1Percentage)).isEqualTo(code1Numeric);
         assertThat(DiscountConverter.fromPercentageToNumeric(code2Percentage)).isEqualTo(code2Numeric);
+        assertThat(DiscountConverter.fromPercentageToNumeric("anyOther")).isEqualTo(codeNumericDefault);
     }
 
     @Test
     void fromCodeToPercentage() {
         assertThat(DiscountConverter.fromCodeToPercentage(code1)).isEqualTo(code1Percentage);
         assertThat(DiscountConverter.fromCodeToPercentage(code2)).isEqualTo(code2Percentage);
+        assertThat(DiscountConverter.fromCodeToPercentage("anyOther")).isEqualTo(codePercentageDefault);
     }
 
     @Test
     void fromCodeToNumeric() {
         assertThat(DiscountConverter.fromCodeToNumeric(code1)).isEqualTo(code1Numeric);
         assertThat(DiscountConverter.fromCodeToNumeric(code2)).isEqualTo(code2Numeric);
+        assertThat(DiscountConverter.fromCodeToNumeric("anyOther")).isEqualTo(codeNumericDefault);
     }
 }
